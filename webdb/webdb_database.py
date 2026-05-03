509df7a5f87a274793817d02595410ed69ae24e4
John Lukach <john@lukach.net>
2026-05-03 23:37:56 +0000

    policy

--- webdb/webdb_database.py
+++ webdb/webdb_database.py
@@ -14,12 +14,47 @@ class WebdbDatabase(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        primary_region = 'us-east-2'
+
         lunker = _ssm.StringParameter.from_string_parameter_attributes(
             self, 'lunker',
             parameter_name = '/account/lunker'
         )
 
-        table = _dynamodb.TableV2(
+        def replica_resource_policy(region: str) -> _iam.PolicyDocument:
+            return _iam.PolicyDocument(
+                statements = [
+                    _iam.PolicyStatement(
+                        sid = 'AllowLunkerAccountQueryAccess',
+                        effect = _iam.Effect.ALLOW,
+                        principals = [
+                            _iam.ArnPrincipal(
+                                f'arn:aws:iam::{lunker.string_value}:root'
+                            )
+                        ],
+                        actions = [
+                            'dynamodb:DescribeTable',
+                            'dynamodb:Query',
+                        ],
+                        resources = [
+                            self.format_arn(
+                                service = 'dynamodb',
+                                region = region,
+                                resource = 'table',
+                                resource_name = 'possibilities',
+                            ),
+                            self.format_arn(
+                                service = 'dynamodb',
+                                region = region,
+                                resource = 'table',
+                                resource_name = 'possibilities/index/*',
+                            ),
+                        ],
+                    )
+                ]
+            )
+
+        _dynamodb.TableV2(
             self, 'possibilities',
             table_name = 'possibilities',
             partition_key = {
@@ -36,31 +71,16 @@ class WebdbDatabase(Stack):
                 point_in_time_recovery_enabled = True
             ),
             deletion_protection = True,
+            resource_policy = replica_resource_policy(primary_region),
             time_to_live_attribute = 'ttl',
             replicas = [
-                _dynamodb.ReplicaTableProps(region = 'us-east-1'),
-                _dynamodb.ReplicaTableProps(region = 'us-west-2'),
+                _dynamodb.ReplicaTableProps(
+                    region = 'us-east-1',
+                    resource_policy = replica_resource_policy('us-east-1'),
+                ),
+                _dynamodb.ReplicaTableProps(
+                    region = 'us-west-2',
+                    resource_policy = replica_resource_policy('us-west-2'),
+                ),
             ],
-        )
-
-        table.add_to_resource_policy(
-            _iam.PolicyStatement(
-                sid = 'AllowLunkerAccountAccess',
-                effect = _iam.Effect.ALLOW,
-                principals = [
-                    _iam.AccountPrincipal(lunker.string_value)
-                ],
-                actions = [
-                    'dynamodb:DescribeTable',
-                    'dynamodb:GetItem',
-                    'dynamodb:Query'
-                ],
-                resources = [
-                    self.format_arn(
-                        service = 'dynamodb',
-                        resource = 'table',
-                        resource_name = 'possibilities'
-                    )
-                ]
-            )
         )
